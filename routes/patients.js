3ac18540f86c4113b9381806a3caa663739b8630
fatima <benyamna2011@outlook.de>
2020-03-23 14:46:53 +0100

    test

--- routes/patients.js
+++ routes/patients.js
@@ -1,1 +1,16 @@
 const express = require ('express')
+
+const router = express.Router();
+const express = require('express');
+
+router.get('/patients/1', patientsControllers (req, res, next){
+  res.send('respond with a resource');
+});
+router.get('/patients/2'), patientsControllers (req, res, next) {
+    res.send('respond with a resource');
+  });
+
+module.exports = router;
+
+
+  
